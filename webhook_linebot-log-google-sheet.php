5aa006325744e88696908403bbbf2054562d3fd9
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2023-09-13 03:20:51 +0700

    Update webhook_linebot-log-google-sheet.php

--- webhook_linebot-log-google-sheet.php
+++ webhook_linebot-log-google-sheet.php
@@ -1,45 +1,32 @@
 <?php
-
 $channelAccessToken = 'nVP4ZB/FEDZU03bs11XhfGBm2VS7ZXAcajKFslpHSDpsDGwr1DpfKqpTHEl5JaiRrK09b2wMPyh4J80OQo8+Zn5+IDcPtMDBnzdlwhrpnpzgC8Zb+YrA2mjav8VlmEgU1IE+1bRt4SO8qet91iHsjgdB04t89/1O/w1cDnyilFU=';
-$gas_url = 'https://script.google.com/macros/s/AKfycbyVFLNU1NR0ijGnc9m8vOCL94E37-BifbTGPEGoVV4iW81XbvN53Fq72PJGJfUrU0HY/exec'; // Replace with your actual GAS web app URL
+$gas_url = 'https://script.google.com/macros/s/AKfycbyVFLNU1NR0ijGnc9m8vOCL94E37-BifbTGPEGoVV4iW81XbvN53Fq72PJGJfUrU0HY/exec';
 
 $request = file_get_contents('php://input');
 $request_json = json_decode($request, true);
 
 foreach ($request_json['events'] as $event) {
-    $user = $event['source']['userId'];
-    $message = $event['message']['text'];
-    $reply_message = 'ฉันได้รับข้อความ "'.$message.'" ของคุณแล้ว!';
+    if ($event['type'] == 'message') {
+        if ($event['message']['type'] == 'text') {
+            $message = $event['message']['text'];
 
-    // Log both the incoming message and the reply message
+            $reply_message = 'ฉันได้รับข้อความ "' . $message . '" ของคุณแล้ว!';
+        } else {
+            $reply_message = 'ฉันได้รับ "' . $event['message']['type'] . '" ของคุณแล้ว!';
+        }
+    } else {
+        $reply_message = 'ฉันได้รับ Event "' . $event['type'] . '" ของคุณแล้ว!';
+    }
+
+    $user = $event['replyToken'];
+
+    reply_message($user, $reply_message);
+
+    // Use the actual data for logMessagesToGoogleAppsScript
     logMessagesToGoogleAppsScript($user, $message, $reply_message);
-
-    // Send the reply message
-    replyMessage($event['replyToken'], $reply_message);
 }
 
-function logMessagesToGoogleAppsScript($user, $message, $reply_message) {
-    global $gas_url;
-    $data = array(
-        'user' => $user,
-        'message' => $message,
-        'reply_message' => $reply_message
-    );
-
-    // Send a POST request to Google Apps Script to log the messages
-    sendPostRequest($gas_url, $data);
-}
-
-function sendPostRequest($url, $data) {
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-    curl_close($ch);
-}
-
-function replyMessage($replyToken, $reply_message) {
+function reply_message($replyToken, $reply_message) {
     global $channelAccessToken;
     $url = 'https://api.line.me/v2/bot/message/reply';
 
@@ -48,5 +35,34 @@ function replyMessage($replyToken, $reply_message) {
 
     $post_header = ['Content-Type: application/json', 'Authorization: Bearer ' . $channelAccessToken];
 
-    sendPostRequest($url, $post_body, $post_header);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
 }
+
+function logMessagesToGoogleAppsScript($user, $message, $reply_message) {
+    global $gas_url;
+
+    // Construct the URL with query parameters
+    $gas_url .= "?user=" . urlencode($user) . "&message=" . urlencode($message) . "&reply_message=" . urlencode($reply_message);
+
+    // Set up cURL for the GET request
+    $ch = curl_init($gas_url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Send the GET request
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+?>
